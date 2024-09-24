152ba895c933d29c83933b287f87df592a37dc47
smanan <smanan0002@gmail.com>
2024-09-24 10:46:37 +0530

    navigate added in email component

--- frontend/src/components/Email.jsx
+++ frontend/src/components/Email.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { MdCropSquare } from 'react-icons/md'
 import { MdOutlineStarBorder } from "react-icons/md";
+import { useNavigate } from 'react-router-dom';
 
 const Email = ({email}) => {
+
+    const navigate = useNavigate();
+    const openMail =() =>{
+        navigate("/mail/1234")
+    }
     
     return (
-        <div className='flex items-center justify-between border-b border-gray-200 px-4 py-3 text-sm hover:cursor-pointer hover:shadow-md'>
+        <div onClick={openMail} className='flex items-center justify-between border-b border-gray-200 px-4 py-3 text-sm hover:cursor-pointer hover:shadow-md'>
             <div className='flex items-center gap-3'>
                 <div className='text-gray-400'>
                     <MdCropSquare size={'20px'} />
