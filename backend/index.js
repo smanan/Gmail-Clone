8a0b87f0d69714dd0edf700ca7fb7261ad011949
smanan <smanan0002@gmail.com>
2024-09-29 11:22:14 +0530

    example how we can create api in and route using express in index.js file

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import express from 'express'
 
-const app = express()
+const app = express();
+
+// app.get("/home",(req,res)=>{
+//     return res.status(200).json({message:'i am coming from backend',success:true})
+// })
 
 const PORT = 8080;
 
