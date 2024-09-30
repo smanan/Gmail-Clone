5e9a92db909ac37905d93dce49cd07cd3f4c03e1
smanan <smanan0002@gmail.com>
2024-09-30 10:26:40 +0530

    email model created

---
+++ backend/models/email.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+
+const emailSchema = new mongoose.Schema({
+    to:{
+        type:String,
+        required:true
+    },
+    subject:{
+        type:String,
+        required:true
+    },
+    message:{
+        type:String,
+        required:true
+    }
+},{timestamps});
+export const  Email = mongoose.model("Email", emailSchema)
