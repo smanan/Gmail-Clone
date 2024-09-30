45977dc76c8b6c7b0bada62d92f06b012c6c6be9
smanan <smanan0002@gmail.com>
2024-09-30 11:20:43 +0530

    middlewares added

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,29 @@
 import express from 'express'
 import dotenv from 'dotenv';
 import connectDB from './db/connectDB.js';
+import cookieParser from 'cookie-parser';
+import cors from "cors"
 
 dotenv.config({});
 connectDB();
 
+const PORT = 8080;
 const app = express();
 
 // app.get("/home",(req,res)=>{
 //     return res.status(200).json({message:'i am coming from backend',success:true})
 // })
 
-const PORT = 8080;
+
+// middlewares
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
+app.use(cookieParser());
+
+const corsOptions = {
+    origin:'http://localhost:5173/',
+    Credentials: true
+}
 
 app.listen(PORT , ()=>{
     console.log(`Server running at port ${PORT}`)
